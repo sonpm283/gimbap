5115513682400bfd3bac0f865df99cc21eaa2489
SonPM <sonpm79382@gmail.com>
2022-07-18 09:08:03 +0700

    refactor main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,76 @@
 //goblal variable
+
+window.addEventListener('DOMContentLoaded', () => {
+    activeCategory.init()
+    fixedMenuCategory.init()
+    scrollSpy.init()
+})
+
+//global variable
 const categoryItem = document.querySelectorAll('.category__item')
+const menuBar = document.querySelector('.menubar')
+const menuHeight = menuBar.getBoundingClientRect().height
+
+//scrollSpy
+const scrollSpy = {
+    init: function() {
+        this.scrollSpy()
+    },
+    scrollSpy: function() {
+        categoryItem.forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                const id = e.currentTarget.dataset.title
+                const element = document.getElementById(id)
+                const fixedMenu = menuBar.classList.contains('menubar--fixed')
+                let position = element.offsetTop - menuHeight
+
+                if(!fixedMenu) {
+                    position = position - menuHeight
+                }
+                window.scroll({
+                    left:0,
+                    top: position
+                })
+            })
+        })
+    }
+}
+
+//fixed menu
+const fixedMenuCategory = {
+    init: function() {
+        this.fixedMenuCategory()
+    },
+    fixedMenuCategory: function() {
+        window.addEventListener('scroll', () => {
+            const scrollHeight = window.pageYOffset
+            if(scrollHeight > menuHeight) {
+                menuBar.classList.add('menubar--fixed')
+            }else {
+                menuBar.classList.remove('menubar--fixed')
+            }
+        })
+    }
+}
 
 //active category menu
-categoryItem.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        categoryItem.forEach(category => {
-            if(category.classList.contains('menu__item--active')) {
-                category.classList.remove('menu__item--active')
-            }
-            e.currentTarget.classList.add('menu__item--active')
+const activeCategory = {
+    init: function() {
+        this.activeCategory()
+    },
+    activeCategory: function() {
+        categoryItem.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                categoryItem.forEach(category => {
+                    if(category.classList.contains('menu__item--active')) {
+                        category.classList.remove('menu__item--active')
+                    }
+                    e.currentTarget.classList.add('menu__item--active')
+                })
+            })
+
         })
-    })
-
-})
-
-//scroll
-categoryItem.forEach(item => {
-
-    item.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        const id = e.currentTarget.dataset.title
-        const element = document.getElementById(id)
-        const menuBar = document.querySelector('.menubar')
-        const menuHeight = menuBar.getBoundingClientRect().height
-        const fixedMenu = menuBar.classList.contains('menubar--fixed')
-        let position = element.offsetTop - menuHeight
-
-        if(!fixedMenu) {
-            position = position - menuHeight
-        }
-        window.scroll({
-            left:0,
-            top: position
-        })
-    })
-})
-
-//fixed nav
-window.addEventListener('scroll', () => {
-    const scrollHeight = window.pageYOffset
-    const menuBar = document.querySelector('.menubar')
-    const menuHeight = menuBar.getBoundingClientRect().height
-    if(scrollHeight > menuHeight) {
-        menuBar.classList.add('menubar--fixed')
-    }else {
-        menuBar.classList.remove('menubar--fixed')
     }
-})
+}
